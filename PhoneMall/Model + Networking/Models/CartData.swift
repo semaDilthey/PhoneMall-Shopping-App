17dd606ffc8661a61fbb8a5da0e73e5782f1f25c
semaDilthey <gaidsemen@yandex.ru>
2023-04-04 19:15:55 +0300

    started models and networking@ git push origin main exit git
    @
    back
    .
    clc
    clr
    /
    //
    :
    %

---
+++ PhoneMall/Model + Networking/Models/CartData.swift
@@ -0,0 +1,21 @@
+//
+//  CartData.swift
+//  PhoneMall
+//
+//  Created by Семен Гайдамакин on 03.04.2023.
+//
+
+import Foundation
+
+struct CartData: Decodable {
+    let basket: [BasketCartData]
+    let delivery: String
+    let total: Int
+}
+
+struct BasketCartData: Decodable {
+    let id: Int
+    let images: String
+    let price: Int
+    let title: String
+}
---
+++ PhoneMall/Model + Networking/Models/ProductDetailsVCData.swift
@@ -0,0 +1,21 @@
+//
+//  ProductDetailsVCData.swift
+//  PhoneMall
+//
+//  Created by Семен Гайдамакин on 03.04.2023.
+//
+
+import Foundation
+
+struct ProductDetailsVCData: Decodable {
+    let title: String
+    let isFavorites: Bool
+    let CPU: String
+    let camera: String
+    let sd: String
+    let ssd: String
+    let capacity: [String]
+    let color: [String]
+    let price: Int
+    let images: [String]
+}
