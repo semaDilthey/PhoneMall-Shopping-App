163ef58906e4cb42cc826c87e79bd0883699c29b
semaDilthey <gaidsemen@yandex.ru>
2023-04-16 23:36:33 +0300

    small fixes

--- PhoneMall/Home/HomeViewModel.swift
+++ PhoneMall/Home/HomeViewModel.swift
@@ -10,8 +10,30 @@ protocol HomeViewModelProtocol {
     func numberOfSections() -> Int
     func numberOfItemsInSection() -> Int
     func cellViewModel(forIndexPath IndexPath: IndexPath) -> CollectionViewCellModelProtocol?
+    func setIndex(setIndex setIndex: IndexPath) -> Int
 }
 
-class HomeViewModel {
+class HomeViewModel : HomeViewModelProtocol {
+  
+    
+    
+    var phones : PhoneManager?
+    
+    func setIndex(setIndex: IndexPath) -> Int {
+        <#code#>
+    }
+    
+    func numberOfSections() -> Int {
+        phones?.phonesArray.count ?? 0
+    }
+    
+    func numberOfItemsInSection() -> Int {
+        <#code#>
+    }
+    
+    func cellViewModel(forIndexPath IndexPath: IndexPath) -> CollectionViewCellModelProtocol? {
+        <#code#>
+    }
+    
     
 }
--- PhoneMall/Model + Networking/PhoneManager.swift
+++ PhoneMall/Model + Networking/PhoneManager.swift
@@ -13,10 +13,9 @@ import UIKit
 //    func didFailWithError(error: Error)
 //}
 
-//class PhoneManager {
-////  Массивчик, в который будет погружаться все что мы парсим
-//    var phonesArray = [HomeVCData]()
-////
+class PhoneManager {
+  //Массивчик, в который будет погружаться все что мы парсим
+    var phonesArray = [HomeVCData]()
 ////    var delegate: PhoneManagerDelegate? //свойство для делегирования по протоколу
 //
 //    let phoneURL = "https://run.mocky.io/v3/654bd15e-b121-49ba-a588-960956b15175"
@@ -41,6 +40,6 @@ import UIKit
 //        .resume()
 //    }
 //
-//}
+}
 
 
